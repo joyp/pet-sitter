675c537aae24ed1160873cd85a27f29c25ea4191
JoyP <joypratt820@gmail.com>
2014-07-16 16:03:23 -0500

    zombie pets, in progress

--- app/models/pet.js
+++ app/models/pet.js
@@ -1,15 +1,59 @@
 'use strict';
 
 function Pet(name, age, gender, species){
-  this.name    = name;
-  this.age     = parseInt(age);
-  this.gender  = gender;
-  this.species = species;
+  this.name     = name;
+  this.age      = parseInt(age);
+  this.gender   = gender;
+  this.species  = species;
+
+  this.isZombie = false;
+  this.wins = 0;
 
   this.health = Math.floor(Math.random() * 91) + 10;
   this.energy = Math.floor(Math.random() * 91) + 10;
   this.full   = Math.floor(Math.random() * 91) + 10;
 }
+
+Pet.prototype.walk = function(){
+  this.health += Math.floor(Math.random() * 3) + 1;
+  this.energy -= Math.floor(Math.random() * 3) + 2;
+  this.full -= Math.floor(Math.random() * 3) + 3; 
+};
+
+Pet.prototype.sleep = function(){
+  this.health += Math.floor(Math.random() * 2) + 2;
+  this.energy += Math.floor(Math.random() * 5) + 1;
+  this.full -= Math.floor(Math.random() * 4) + 4; 
+};
+
+Pet.prototype.eat= function(){
+  this.health += Math.floor(Math.random() * 4) + 1;
+  this.energy -= Math.floor(Math.random() * 6) + 3;
+  this.full += Math.floor(Math.random() * 5) + 5; 
+};
+
+Pet.prototype.attack = function(pet){
+  var damage = Math.random() * 5;
+
+  if(!this.isZombie){
+  damage += (this.health / 10) + (this.energy / 20) + (this.full / 30);
+  }
+
+  pet.health -= damage;
+
+  if((pet.health < 0) && !pet.isZombie){
+    pet.isZombie = true;
+    this.wins++;
+  }
+};
+
+Pet.prototype.resurrect = function(){
+  if(this.isZombie && (this.wins >= 5)){
+    this.health = Math.floor(Math.random() * 26) + 25;
+    this.wins -= 5;
+    this.isZombie = false;
+  }
+};
 
 module.exports = Pet;
 
